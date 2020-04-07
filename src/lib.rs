4f31fc2a6f50cf256e70cdac3d747f514d1a3d47
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-04-07 04:15:54 +0200

    Expose `wgpu` coordinate system directly

--- src/lib.rs
+++ src/lib.rs
@@ -434,9 +434,9 @@ fn orthographic_projection(width: u32, height: u32) -> [f32; 16] {
     #[cfg_attr(rustfmt, rustfmt_skip)]
     [
         2.0 / width as f32, 0.0, 0.0, 0.0,
-        0.0, 2.0 / height as f32, 0.0, 0.0,
+        0.0, -2.0 / height as f32, 0.0, 0.0,
         0.0, 0.0, 1.0, 0.0,
-        -1.0, -1.0, 0.0, 1.0,
+        -1.0, 1.0, 0.0, 1.0,
     ]
 }
 
