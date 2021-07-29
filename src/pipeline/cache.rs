5ab31779c384977e1d44bf09daed145b792155fc
profan <robinhubner@gmail.com>
2021-07-29 20:56:49 +0100

    remove use of padding as not necessary for Queue::write_texture

--- src/pipeline/cache.rs
+++ src/pipeline/cache.rs
@@ -2,13 +2,10 @@ use std::num::NonZeroU32;
 
 pub struct Cache {
     texture: wgpu::Texture,
-    pub(super) view: wgpu::TextureView,
-    padded_cache: Vec<u8>
+    pub(super) view: wgpu::TextureView
 }
 
 impl Cache {
-    const INITIAL_UPLOAD_BUFFER_SIZE: u64 =
-        wgpu::COPY_BYTES_PER_ROW_ALIGNMENT as u64 * 100;
 
     pub fn new(device: &wgpu::Device, width: u32, height: u32) -> Cache {
         let texture = device.create_texture(&wgpu::TextureDescriptor {
@@ -27,13 +24,9 @@ impl Cache {
 
         let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
 
-        let mut padded_cache = Vec::new();
-        padded_cache.reserve(Self::INITIAL_UPLOAD_BUFFER_SIZE as usize);
-
         Cache {
             texture,
-            view,
-            padded_cache
+            view
         }
     }
 
@@ -49,24 +42,6 @@ impl Cache {
         let width = size[0] as usize;
         let height = size[1] as usize;
 
-        // It is a webgpu requirement that:
-        //  BufferCopyView.layout.bytes_per_row % wgpu::COPY_BYTES_PER_ROW_ALIGNMENT == 0
-        // So we calculate padded_width by rounding width
-        // up to the next multiple of wgpu::COPY_BYTES_PER_ROW_ALIGNMENT.
-        let align = wgpu::COPY_BYTES_PER_ROW_ALIGNMENT as usize;
-        let padded_width_padding = (align - width % align) % align;
-        let padded_width = width + padded_width_padding;
-
-        let padded_data_size = (padded_width * height) as u64;
-        self.padded_cache.resize(padded_data_size as usize, 0);
-
-        let padded_data = self.padded_cache.as_mut_slice();
-
-        for row in 0..height {
-            padded_data[row * padded_width..row * padded_width + width]
-                .copy_from_slice(&data[row * width..(row + 1) * width]);
-        }
-
         queue.write_texture(
             wgpu::ImageCopyTexture {
                 texture: &self.texture,
@@ -77,10 +52,10 @@ impl Cache {
                     z: 0,
                 },
             },
-            &padded_data,
+            &data,
             wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: NonZeroU32::new(padded_width as u32),
+                bytes_per_row: NonZeroU32::new(width as u32),
                 rows_per_image: NonZeroU32::new(height as u32),
             },
             wgpu::Extent3d {
