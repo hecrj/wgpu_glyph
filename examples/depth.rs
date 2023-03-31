50e86a73746c424f00034c1ef2b520b24c364f5b
Héctor Ramón Jiménez <hector0193@gmail.com>
2023-03-31 15:17:21 +0200

    Use `?` instead of `expect` when creating a surface

--- examples/depth.rs
+++ examples/depth.rs
@@ -17,7 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
     let surface =
-        unsafe { instance.create_surface(&window) }.expect("Create surface");
+        unsafe { instance.create_surface(&window)? };
 
     // Initialize GPU
     let (device, queue) = futures::executor::block_on(async {
